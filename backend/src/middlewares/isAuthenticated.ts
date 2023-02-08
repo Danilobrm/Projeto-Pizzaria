b711d2de97483bdf18bd44bfaa960cc921fcd5f3
danilobrm <danilobr.web@gmail.com>
2023-02-08 17:22:15 -0300

    autenticacao check middleware

---
+++ backend/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface Payload {
+  sub: string;
+}
+
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  // receber o token
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    return res.status(401).end();
+  }
+
+  const [, token] = authToken.split(' ');
+
+  try {
+    // validar o token
+    const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
+
+    // recuperar o id do token e colocar dentro
+    // de uma variável user_id dentro do req
+    req.user_id = sub;
+
+    return next();
+  } catch (error) {
+    return res.status(401).end();
+  }
+}
