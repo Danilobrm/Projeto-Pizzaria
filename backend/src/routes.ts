5422d60fde51c16b15c11d89fd7ee786feeec8b2
danilobrm <danilobr.web@gmail.com>
2023-02-08 17:20:17 -0300

    rota sobre usuario

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
@@ -8,5 +11,7 @@ const router = Router();
 router.post('/users', new CreateUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
+
+router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 export { router };
