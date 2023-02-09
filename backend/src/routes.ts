da9122509364c6ba77f8d3c728c4c18c5ac140c2
danilobrm <danilobr.web@gmail.com>
2023-02-09 15:11:33 -0300

    routes product, file and order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+
 import { CreateProductController } from './controllers/product/CreateProductController';
-import { AuthUserController } from './controllers/user/AuthUserController';
-import { CreateUserController } from './controllers/user/CreateUserController';
-import { DetailUserController } from './controllers/user/DetailUserController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+
+import { CreateOrderController } from './controllers/order/CreateOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
+import uploadConfig from './config/multer';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload('./tmp'));
 
 // ROTAS USER
 router.post('/users', new CreateUserController().handle);
@@ -27,6 +39,21 @@ router.post(
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // ROTAS PRODUCT
-router.post('/product', isAuthenticated, new CreateProductController().handle);
+router.post(
+  '/product',
+  isAuthenticated,
+  upload.single('file'),
+  new CreateProductController().handle,
+);
+
+router.get(
+  '/category/product',
+  isAuthenticated,
+  new ListByCategoryController().handle,
+);
+
+// ROTAS ORDER
+router.post('/order', isAuthenticated, new CreateOrderController().handle);
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 
 export { router };
