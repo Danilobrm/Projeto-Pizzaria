7bdf63691dc367e19f0234c6bfa4a3946d044611
danilobrm <danilobr.web@gmail.com>
2023-02-09 21:59:16 -0300

    routes add and remove item

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,11 +12,14 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 import uploadConfig from './config/multer';
-import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 
 const router = Router();
 
@@ -55,5 +58,12 @@ router.get(
 // ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
+router.delete(
+  '/order/remove',
+  isAuthenticated,
+  new RemoveItemController().handle,
+);
 
 export { router };
