6ba5d64ca66f63eb3fc2d7d9b04412b92ac31b8c
danilobrm <danilobr.web@gmail.com>
2023-02-13 21:32:22 -0300

    initial commit frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Trattoria Trussardi - Faça seu Login</title>
+      </Head>
+      <div>Trattoria Trussardi</div>
+    </>
+  );
+}
